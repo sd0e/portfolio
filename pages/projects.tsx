120a9d69641e7ac7219c680b51fe7a20d03e1f41
sd0e <sebastiandoe5@gmail.com>
2023-03-24 23:56:25 +1100

    Implement fetching algorithm for all projects (`page`)

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -2,10 +2,20 @@ import Head from 'next/head';
 import { Inter } from 'next/font/google';
 import Layout from '@/components/layout';
 import { useRouter } from 'next/router';
+import { getSortedProjects } from '@/lib/projects';
+import { GetStaticProps } from 'next/types';
 
 const inter = Inter({ subsets: ['latin'], weight: ['600', '700'] })
 
-export default function Projects() {
+export default function Projects({ projects }: { projects: {
+  id: string,
+  name: string,
+  languages: Array<string>,
+  month: string,
+  year: number,
+  link: string,
+  priority: number
+}}) {
   const router = useRouter();
   const pathname = router.pathname;
 
@@ -15,6 +25,16 @@ export default function Projects() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      main content
     </Layout>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const projects = await getSortedProjects();
+  return {
+      props: {
+          projects
+      }
+  }
 }
