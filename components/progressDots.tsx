3a9926e384a1653abc3e79598de211533621c939
sd0e <sebastiandoe5@gmail.com>
2023-07-28 13:57:16 +0100

    Add key property

--- components/progressDots.tsx
+++ components/progressDots.tsx
@@ -3,6 +3,6 @@ import { Stack } from '@mui/material';
 
 export default function ProgressDots({ num, selected } : { num: number, selected: number }) {
     return <Stack direction="row" spacing={1}>
-        {[...Array(num)].map((_, i) => <div className={i + 1 === selected ? [styles.dot, styles.dotSelected].join(' ') : styles.dot}></div>)}
+        {[...Array(num)].map((_, i) => <div className={i + 1 === selected ? [styles.dot, styles.dotSelected].join(' ') : styles.dot} key={i}></div>)}
     </Stack>
 }
