29a974289be28e279d6a0f44b6b933a03b3428fc
sebastiandoe5 <sebastiandoe5@gmail.com>
2024-02-21 19:08:36 +0000

    Add tracking code

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script defer src="https://us.umami.is/script.js" data-website-id="fa662e36-93fc-42fb-8d69-461104ff7dc5"></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
