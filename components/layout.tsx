a68a45d6afc403e7bdc7b242e89e879fd6a1a1bd
sebastiandoe5 <sebastiandoe5@gmail.com>
2025-01-17 16:33:48 +0000

    Attempt to fix lag issue

--- components/layout.tsx
+++ components/layout.tsx
@@ -37,7 +37,7 @@ export default function Layout({ children, mainPage = false, headerOnly = false,
     }
 
     if (typeof window !== 'undefined') {
-        window.addEventListener('keydown', e => {
+        window.addEventListener('keyup', e => {
             if (e.key === 'ArrowLeft' && previousPageLink) movePreviousPageLink();
         })
     }
